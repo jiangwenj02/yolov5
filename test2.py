9b89a933a1cc8afe8524e00c636f8942f23471b9
jiangwenj02 <jiangwenj02@gmail.com>
2021-06-06 15:47:24 +0800

    debug

--- test2.py
+++ test2.py
@@ -185,7 +185,7 @@ def test(data,
                 box[:, :2] -= box[:, 2:] / 2  # xy center to top-left corner
                 for p, b in zip(pred.tolist(), box.tolist()):
                     jdict.append({'image_id': image_id,
-                                  'category_id': coco91class[int(p[5])] if is_coco else int(p[5]),
+                                  'category_id': int(p[5]),
                                   'bbox': [round(x, 3) for x in b],
                                   'score': round(p[4], 5)})
 
