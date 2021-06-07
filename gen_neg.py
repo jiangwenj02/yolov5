255904ab073071ec512d81f532fd77a24c95f4c0
MyCurveNet <137376864@qq.com>
2021-06-07 20:14:25 +0800

    fix

--- gen_neg.py
+++ gen_neg.py
@@ -12,7 +12,7 @@ import matplotlib as mpl
 import multiprocessing
 import cv2
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
+from utils.datasets import LoadStreams, LoadVideos
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
 from utils.plots import colors, plot_one_box
@@ -84,7 +84,7 @@ class Evaluator:
             if not os.path.isfile(video_path):
                 print('{} not exist'.format(video_path))
                 continue
-            dataset = LoadImages(video_path, img_size=self.opt.imgsz, stride=self.opt.stride)
+            dataset = LoadVideos(video_path, img_size=self.opt.imgsz, stride=self.opt.stride)
             for path, img, im0s, vid_cap in dataset:
                 img = torch.from_numpy(img).to(self.device)
                 img = img.half() if self.half else img.float()  # uint8 to fp16/32
@@ -415,7 +415,7 @@ if __name__ == '__main__':
     parser.add_argument('--start', default=0, type=int,  help="video index to start")
     parser.add_argument('--end', default=0, type=int, help="video index to end")
     args = parser.parse_args()
-    # evaluator = Evaluator()
+    evaluator = Evaluator()
     csv = CSV_helper_gastric()
     csv.open_csv('neg.csv')
 
@@ -427,6 +427,6 @@ if __name__ == '__main__':
     end_index = args.end
     print(gt)
 
-    # evaluator.test_video2(gt,start_video_index,end_index)
+    evaluator.test_video(gt,start_video_index,end_index)
 
 
