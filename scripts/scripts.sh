e62d9bec6b56a4a26850907e68e614c7c5371c5d
jiangwenj02 <jiangwenj02@gmail.com>
2021-07-13 12:48:10 +0800

    fix 0.4

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -1,2 +1,2 @@
-python test2.py --weights runs/train/exp4/weights/best.pt --save-json --data data/coco.yaml --conf-thres 0.5
-python gen_neg.py --csv_file /data3/zzhang/annotation/erosiveulcer_fine/neg0615.csv --video_path /data3/zzhang/tmp/videos/ --save_path /data3/zzhang/tmp/erosive_ulcer_videos0707/
+python test2.py --weights runs/train/exp4/weights/best.pt --save-json --data data/coco.yaml --conf-thres 0.5 --ann /data3/zzhang/annotation/erosiveulcer_fine/test.json
+python gen_neg.py --csv_file /data3/zzhang/annotation/erosiveulcer_fine/neg0615.csv --video_path /data3/zzhang/tmp/videos/ --save_path /data3/zzhang/tmp/erosive_ulcer_videos0712_th03/ --weights runs/train/exp11/weights/best.pt
