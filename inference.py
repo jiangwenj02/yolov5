9dc63b2a79f10b6dc717e452ddabf002de03ca23
jiangwenj02 <jiangwenj02@gmail.com>
2021-08-04 22:51:56 +0800

    fix

--- inference.py
+++ inference.py
@@ -59,5 +59,5 @@ class YoloBase(metaclass=ABCMeta):
 
 
 if __name__ == '__main__':
-    detector = YoloBase('./', 'custom', './runs/train/exp4/weights/best.pt', 'runs/hub/exp')
+    detector = YoloBase('./', './runs/train/exp4/weights/best.pt', 'runs/hub/exp')
     detector.predict('/home1/users/jiangwenj02/mmdetection/data/erosiveulcer/images/00088057-49db-4200-ad48-4a011b0ff906.jpg')
