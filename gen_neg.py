4d7a288ee5ad2eb885b64115afe4d5f2fc0566c8
jiangwenj02 <jiangwenj02@gmail.com>
2021-11-03 01:06:03 +0800

    f

--- gen_neg.py
+++ gen_neg.py
@@ -303,10 +303,10 @@ class CSV_helper_gastric(object):
                         try:
                             start = datetime.datetime.strptime(row_cell[0], '%M:%S').time()
                             end = datetime.datetime.strptime(row_cell[1], '%M:%S').time()
+                        except ValueError:
+                            start = datetime.datetime.strptime(row_cell[0], '%H:%M:%S').time()
+                            end = datetime.datetime.strptime(row_cell[1], '%H:%M:%S').time()
                         except:
-                            print(row_cell)
-                            import pdb
-                            pdb.set_trace()
                             continue
 
                         start_second = (start.hour * 60 + start.minute) * 60 + start.second
