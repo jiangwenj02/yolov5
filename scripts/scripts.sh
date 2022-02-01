7861effd9a049c0d4582983589f96655d5b9f687
jiangwenj02 <jiangwenj02@gmail.com>
2022-02-01 22:30:46 +0800

    fix

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -22,11 +22,11 @@ python gen_neg.py --csv_file /data3/zzhang/tmp/anno1228.csv --video_path /data3/
 ####gen video empty anno
 python gen_anno.py --dirs /data2/zinan_xiong/gastritis_videos_for_test/ --hz mp4 --out /data3/zzhang/tmp/anno_cls_0104.csv
 python gen_neg_cls.py --csv_file /data3/zzhang/tmp/anno_cls_0125.csv  --video_path /data2/zinan_xiong/gastritis_videos_for_test_2/ \
---save_path ../mmclassification/work_dirs/shuffle_stomach_mix_hr/gastritis_videos_for_test2/ \
+--save_path ../mmclassification/work_dirs/shuffle_stomach_mix_hr/gastritis_videos_for_test2_0201/ \
 --weights /data3/zzhang/mmclassification/work_dirs/shuffle_stomach_mix_hr/latest.pth \
 --config /data3/zzhang/mmclassification/configs/diseased/shuffle_stomach_mix_hr.py
 
 python gen_neg_cls.py --csv_file /data3/zzhang/tmp/anno_cls_0125.csv  --video_path /data2/zinan_xiong/gastritis_videos_for_test_2/ \
---save_path ../mmclassification/work_dirs/seresnet50_b32x8_stomach_mix_hr/gastritis_videos_for_test2/ \
+--save_path ../mmclassification/work_dirs/seresnet50_b32x8_stomach_mix_hr/gastritis_videos_for_test2_0201/ \
 --weights /data3/zzhang/mmclassification/work_dirs/seresnet50_b32x8_stomach_mix_hr/latest.pth \
 --config /data3/zzhang/mmclassification/configs/diseased/seresnet50_b32x8_stomach_mix_hr.py
